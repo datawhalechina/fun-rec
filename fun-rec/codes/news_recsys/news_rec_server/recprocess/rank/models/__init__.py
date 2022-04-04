9027c97f313189ea69009d734c4eb5ff9a733b5e
cheater <615922749@qq.com>
2022-04-04 22:38:36 +0800

    更新面经参考答案

---
+++ fun-rec/codes/news_recsys/news_rec_server/recprocess/rank/models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @File  : __init__.py.py
+# @Author: xLyons
+# @IDE   : PyCharm
+# @Time  : 2022/1/27
---
+++ fun-rec/codes/news_recsys/news_rec_web/vue3-fun-rec/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+</template>
+
+<style lang="less">
+ * {
+    padding: 0;
+    margin: 0;
+    border: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body,
+  html {
+    width: 100%;
+    height: 100%;
+    /* overflow: hidden; */
+  }
+
+  /* 去掉获取焦点时的边框 */
+  input {
+    background: none;
+    border: none;
+    outline: none;
+    display: black;
+  }
+
+  /* 登录注册页面输入框 */
+  :deep(.van-cell__value:hover),
+  :deep(.van-cell__value:focus) {
+    color: white;
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    background-color: transparent;
+  }
+</style>
